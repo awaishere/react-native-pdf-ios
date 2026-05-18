5759cd70907bd13d25e43396448379cbacf082d6
awaishere <awaishere007@gmail.com>
2026-05-19 01:51:41 +0500

    podspec updated again

--- FabricExample/react-native.config.js
+++ FabricExample/react-native.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  dependency: {
-    platforms: {
-      android: null,
-    },
-  },
   project: {
     ios: { sourceDir: 'ios' },
   },
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: {
+        podspecPath: './react-native-pdf-ios.podspec',
+      },
+      android: null,
+    },
+  },
+};
